515652243ca0c502df7dd4944b426a5c3a4344bd
Herny Lee <47008435+egoleecode@users.noreply.github.com>
2024-01-25 00:02:07 +0800

    Important update
    1. Completely updated the validation logic for enhanced security.
    2. Introduced Drand as a supplementary randomness source.

--- test_tools/answer_challenge.py
+++ test_tools/answer_challenge.py
@@ -1,5 +1,7 @@
-import requests,json,random,base64,time
-cid = ""
+import requests,json,base64,time
+from collections import deque
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
 IPFS_API_URL = "http://127.0.0.1:5010"
 IPFS_GATEWAY_URL = "http://127.0.0.1:8080"
 
@@ -8,23 +10,47 @@ def get_leaf_block(cid:str):
     url=f"{IPFS_API_URL}/api/v0/ls?arg=%s&headers=true&size=true" % cid
     cids = eval(requests.post(url).text)['Objects'][0]['Links']
     return cids
-#获得某个CID的所有末端叶子块
-def get_extremity_leaf_block_list(cid: str):
-    queue = [cid]
-    leaf_blocks_list = []
 
-    while queue:
-        current_cid = queue.pop(0)
-        url = f"{IPFS_API_URL}/api/v0/ls?arg={current_cid}&headers=true&size=true"
-        response = requests.post(url)
-        links = response.json()['Objects'][0]['Links']
+#获得某个CID的所有末端叶子块 - 多线程
+def process_block(cid):
+    block_data = get_leaf_block(cid)
+    result = []
 
-        for link in links:
-            if link['Type'] == 2 and int(link['Size']) <= 262144:
-                if not get_leaf_block(link['Hash']):  # 如果没有子块，则为叶子块
-                    leaf_blocks_list.append(link['Hash'])
+    for i in block_data:
+        if i['Type'] == 1:
+            result.append(i['Hash'])
+        elif i['Type'] == 2:
+            if int(i['Size']) <= 262144:
+                if get_leaf_block(i['Hash']) == []:
+                    result.append(i['Hash'])
+                else:
+                    result.extend([leaf['Hash'] for leaf in get_leaf_block(i['Hash'])])
             else:
-                queue.append(link['Hash'])
+                result.extend([leaf['Hash'] for leaf in get_leaf_block(i['Hash'])])
+    
+    return result
+#获得某个CID的所有末端叶子块 - 多线程
+def get_extremity_leaf_block_list(cid: str, max_workers=10):
+    leaf_blocks_list = []
+    queue = deque([cid])
+    visited = set()
+
+    with ThreadPoolExecutor(max_workers=max_workers) as executor:
+        while queue:
+            future_to_cid = {executor.submit(process_block, current_cid): current_cid for current_cid in queue}
+            queue.clear()
+            for future in as_completed(future_to_cid):
+                try:
+                    result = future.result()
+                    for item in result:
+                        if item not in visited:
+                            queue.append(item)
+                            visited.add(item)
+                            if get_leaf_block(item) == []:
+                                leaf_blocks_list.append(item)
+                except Exception as e:
+                    print(f"Error processing block: {e}")
+
     return leaf_blocks_list
 
 #将末端叶子块转化为Base64形式编码
@@ -63,6 +89,6 @@ answer = answer_challenge(challenge)
 end_time = time.time()
 
 if answer == challenge['answer']:
-    print("挑战验证成功！耗时:", end_time - start_time)
+    print("挑战验证成功！耗时:", end_time - start_time, "s")
 else:
     print("挑战验证失败！")
--- test_tools/get_challenge.py
+++ test_tools/get_challenge.py
@@ -1,31 +1,51 @@
 import requests,json,random,base64,time
-cid = ""
+
 IPFS_API_URL = "http://127.0.0.1:5010"
 IPFS_GATEWAY_URL = "http://127.0.0.1:8080"
+DRAND_URL = "https://drand.cloudflare.com/8990e7a9aaed2ffed73dbd7092123d6f289930540d7651336225dc172e51b2ce/public/latest"
 
 #获得某个CID的叶子块
 def get_leaf_block(cid:str):
     url=f"{IPFS_API_URL}/api/v0/ls?arg=%s&headers=true&size=true" % cid
     cids = eval(requests.post(url).text)['Objects'][0]['Links']
     return cids
-#获得某个CID的所有末端叶子块
-def get_extremity_leaf_block_list(cid: str):
-    queue = [cid]
+
+#从drand获得最新的随机数
+def get_random_number():
+    url = f"{DRAND_URL}"
+    response = requests.get(url)
+    data = response.json()
+    return int(data["randomness"], 16)
+
+#获得随机的n个末端叶子块
+def get_random_leaf_block_list(cid: str, max_leaf_blocks: int):
+    not_leaf_blocks_list = get_leaf_block(cid)
     leaf_blocks_list = []
 
-    while queue:
-        current_cid = queue.pop(0)
-        url = f"{IPFS_API_URL}/api/v0/ls?arg={current_cid}&headers=true&size=true"
-        response = requests.post(url)
-        links = response.json()['Objects'][0]['Links']
-
-        for link in links:
-            if link['Type'] == 2 and int(link['Size']) <= 262144:
-                if not get_leaf_block(link['Hash']):  # 如果没有子块，则为叶子块
-                    leaf_blocks_list.append(link['Hash'])
+    while not_leaf_blocks_list and len(leaf_blocks_list) < max_leaf_blocks:
+        current_block = random.choice(not_leaf_blocks_list)
+        # 检查是否是末端叶子块
+        if current_block['Type'] == 2 and int(current_block['Size']) <= 262144 and current_block['Hash'] not in not_leaf_blocks_list:
+            if get_leaf_block(current_block['Hash']) == []:
+                leaf_blocks_list.append(current_block['Hash'])
             else:
-                queue.append(link['Hash'])
+                # 如果不是末端叶子块，决定是否进入
+                if random.choice([True, False]):
+                    leafs = get_leaf_block(current_block['Hash'])
+                    not_leaf_blocks_list.extend(leafs)
+        elif current_block['Type'] == 1:
+            if random.choice([True, False]):
+                leafs = get_leaf_block(current_block['Hash'])
+                not_leaf_blocks_list.extend(leafs)
+    print(leaf_blocks_list)
     return leaf_blocks_list
+
+#选取一个末端叶子块
+def select_leaf_blocks_by_drand(leaf_blocks_list):
+    leaf_blocks_list.sort()  # 按字母顺序排序
+    round_number = get_random_number()
+    selected_index = round_number % len(leaf_blocks_list)
+    return leaf_blocks_list[selected_index]
 
 #将末端叶子块转化为Base64形式编码
 def get_block_base64(cid:str):
@@ -40,21 +60,22 @@ def devide_raw_base64(text: str):
     return a_base64, b_base64
 
 #生成挑战内容
-def creat_challenge(cid:str)->dict:
+def creat_challenge(cid: str,max_leaf_blocks: int)->dict:
     challenge = {}
     challenge['cid'] = cid
-    challenge_base64_raw = list(devide_raw_base64(get_block_base64(random.choice(get_extremity_leaf_block_list(cid)))))
+    challenge_base64_raw = list(devide_raw_base64(get_block_base64(select_leaf_blocks_by_drand(get_random_leaf_block_list(cid, max_leaf_blocks)))))
     challenge['question'] = random.choice(challenge_base64_raw)
     challenge_base64_raw.remove(challenge['question'])
     challenge['answer'] = challenge_base64_raw[0]
     return challenge
 
 cid = ""
+max_leaf_blocks = 10 #最大叶子块数量
 
 start_time = time.time()
-challenge = creat_challenge(cid)
+challenge = creat_challenge(cid,max_leaf_blocks)
 # 保存挑战到JSON文件
 with open('challenge.json', 'w') as file:
     json.dump(challenge, file)
 end_time = time.time()
-print("挑战已生成并保存到 'challenge.json'耗时:", end_time - start_time)
+print("挑战已生成并保存到 'challenge.json'耗时:", end_time - start_time, "s")
